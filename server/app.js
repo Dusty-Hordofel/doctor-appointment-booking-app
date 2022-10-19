da8ebca82109a0c8cb100fc567081f50769d597f
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2022-10-19 11:32:57 +0200

    update folders

--- server/app.js
+++ server/app.js
@@ -1,38 +1,30 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
-// const morgan = require("morgan");
-// const cors = require("cors");
-// const path = require("path");
-// const app = express();
-// dotenv.config();
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+dotenv.config();
 
-// const dbConfig = require("./config/db");
+const dbConfig = require("./config/db");
 
-// //Routes
-// const userRoutes = require("./routes/userRoute");
-// const adminRoute = require("./routes/adminRoute");
-// const doctorRoute = require("./routes/doctorsRoute");
+//Routes
+const userRoutes = require("./routes/userRoute");
+const adminRoute = require("./routes/adminRoute");
+const doctorRoute = require("./routes/doctorsRoute");
 
-// app.use(morgan("dev"));
-// app.use(cors());
-// app.use(express.json({ limit: "5mb" }));
-// app.use(express.urlencoded({ limit: "5mb", extended: true }));
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
-// const PREFIX = "/" + process.env.PREFIX;
+const PREFIX = "/" + process.env.PREFIX;
 
-// app.use(PREFIX, userRoutes);
-// app.use("/api/admin", adminRoute);
-// app.use("/api/doctor", doctorRoute);
+app.use(PREFIX, userRoutes);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorRoute);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use("/", express.static("client/build"));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-//   });
-// }
-
-// const port = process.env.PORT || 3810;
-// app.listen(port, () => {
-//   console.log(`Server started on port ${port}`);
-// });
+const port = process.env.PORT || 3810;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
