a8283b2c427071c0413dca6b475d44de7cfe1ddb
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2022-10-19 11:34:58 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const cors = require("cors");
 const path = require("path");
 const app = express();
@@ -13,7 +13,7 @@ const userRoutes = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
 
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
